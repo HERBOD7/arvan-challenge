8d7a3d02e20fc4db9fa43ac2de6289336e40c264
HERBOD7 <herbodabdoli@gmail.com>
2020-12-25 19:43:05 +0330

    feat: add login page

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from "vue";
 import App from "./App";
+import { BootstrapVue } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import "@/styles/app.css";
 
 Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
 
 new Vue({
   render: h => h(App)
