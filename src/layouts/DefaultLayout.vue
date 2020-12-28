157c10aae426da38c0ab8533fba2cef77cee4aa5
HERBOD7 <herbodabdoli@gmail.com>
2020-12-28 21:22:48 +0330

    feat: store user info

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,3 +1,14 @@
+<script>
+import storage from '@/utils/storage';
+export default {
+  created() {
+    if (storage.getItem('user')) {
+      this.$store.commit('SET_USER', storage.getItem('user'));
+    }
+  }
+};
+</script>
+
 <template>
   <div>
     <RouterView />
