8ce93dfef0893a86cfa0f86ad078fdb66b8e9c0c
HERBOD7 <herbodabdoli@gmail.com>
2020-12-28 17:14:59 +0330

    fix: fix register layout

--- src/layouts/Register.vue
+++ src/layouts/Register.vue
@@ -1,50 +1,46 @@
-<template>
-  <div
-    class="d-flex flex-column justify-content-center align-items-center vh-100"
-  >
-    <b-form class="bg-light auth-form rounded" @submit.stop.prevent>
-      <p class="text-center display-4 text-secondary">Register</p>
-      <InputField
-        id="user"
-        v-model="userName"
-        class="mt-48"
-        title="User"
-        required="true"
-      />
-
-      <InputField
-        id="email"
-        v-model="emailAddress"
-        class="mt-25"
-        title="Email"
-        required="true"
-      />
-
-      <InputField
-        id="password"
-        v-model="password"
-        class="mt-25"
-        title="Password"
-        required="true"
-      />
-
-      <Button type="submit" class="mt-48 w-100 btn-primary" text="Register" />
-      <div class="d-flex mt-3">
-        <p class="fz-16">Already Registered?</p>
-        <a href="/login" class="font-weight-bold ml-11 fz-16">Login</a>
-      </div>
-    </b-form>
-  </div>
-</template>
-
 <script>
 import InputField from "@/components/auth/InputField";
-import Button from "@/components/shared/Button";
+import BaseButton from "@/components/shared/BaseButton";
 export default {
   name: "LogIn",
   components: {
     InputField,
-    Button
+    BaseButton
   }
 };
 </script>
+
+<template>
+  <form class="bg-light auth-form rounded" @submit.stop.prevent>
+    <p class="text-center display-4 text-secondary">Register</p>
+    <InputField
+      id="user"
+      v-model="userName"
+      class="mt-48"
+      title="User"
+      required="true"
+    />
+
+    <InputField
+      id="email"
+      v-model="emailAddress"
+      class="mt-25"
+      title="Email"
+      required="true"
+    />
+
+    <InputField
+      id="password"
+      v-model="password"
+      class="mt-25"
+      title="Password"
+      required="true"
+    />
+
+    <BaseButton type="submit" class="mt-48 w-100 btn-primary" text="Register" />
+    <div class="d-flex mt-3">
+      <p class="fz-16">Already Registered?</p>
+      <a href="/login" class="font-weight-bold ml-11 fz-16">Login</a>
+    </div>
+  </form>
+</template>
