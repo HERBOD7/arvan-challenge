dae6c77f85b2f1637894dfb0f812c82c8abdda90
HERBOD7 <herbodabdoli@gmail.com>
2020-12-28 17:32:59 +0330

    feat: add layout resolver and auth layout

---
+++ src/layouts/Auth.vue
@@ -0,0 +1,14 @@
+<script>
+// import LogIn from "@/views/LogIn";
+export default {
+  name: "Auth"
+};
+</script>
+
+<template>
+  <div
+    class="d-flex flex-column justify-content-center align-items-center vh-100"
+  >
+    <RouterView />
+  </div>
+</template>
