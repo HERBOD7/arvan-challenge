926abee1b9ada469f64149c7b7145a725e1b8d3f
HERBOD7 <herbodabdoli@gmail.com>
2020-12-29 23:35:20 +0330

    feat: add fetch tag to posts module

--- src/api/modules/posts.js
+++ src/api/modules/posts.js
@@ -1,16 +1,27 @@
-import $http from '@/services/http';
+import $http from "@/services/http";
 
-const FETCH_POSTS_ENDPOINT = '/articles';
-const ADD_POST_ENDPOINT = '/articles';
-const EDIT_POST_ENDPOINT = (id) => `/articles/${id}`;
-const DELETE_POSTS_ENDPOINT = (id) => `/posts/${id}`;
+const FETCH_POSTS_ENDPOINT = "/articles";
+const ADD_POST_ENDPOINT = "/articles";
+const TAGS_ENDPOINT = "/tags";
+const EDIT_POST_ENDPOINT = id => `/articles/${id}`;
+const DELETE_POSTS_ENDPOINT = id => `/posts/${id}`;
 
 export default {
   fetchPosts() {
     return $http.get(FETCH_POSTS_ENDPOINT);
   },
-  addPost(body) {
-    return $http.post(ADD_POST_ENDPOINT, body);
+  fetchTags() {
+    return $http.get(TAGS_ENDPOINT);
+  },
+  addPost(title, description, body, tagList) {
+    return $http.post(ADD_POST_ENDPOINT, {
+      article: {
+        title,
+        description,
+        body,
+        tagList
+      }
+    });
   },
   editPost(id, body) {
     return $http.put(EDIT_POST_ENDPOINT(id), body);
