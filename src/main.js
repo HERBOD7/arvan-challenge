29a3677a677869075ec42d5669e54920e27148a9
HERBOD7 <herbodabdoli@gmail.com>
2021-01-02 15:26:41 +0330

    fix: fix notification

--- src/main.js
+++ src/main.js
@@ -4,10 +4,17 @@ import { BootstrapVue } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import "@/styles/app.css";
+import router from "./router";
+import "@/api";
+import store from "./store";
+import Notifications from "vue-notification";
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
+Vue.use(Notifications);
 
 new Vue({
+  router,
+  store,
   render: h => h(App)
 }).$mount("#app");
