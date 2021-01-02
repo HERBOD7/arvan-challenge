8e6a1e2ec50c1d075ea1de636793edc032241124
HERBOD7 <herbodabdoli@gmail.com>
2021-01-02 21:05:13 +0330

    fix: fix fetch post

--- src/api/modules/posts.js
+++ src/api/modules/posts.js
@@ -3,12 +3,15 @@ import $http from "@/services/http";
 const FETCH_POSTS_ENDPOINT = "/articles";
 const ADD_POST_ENDPOINT = "/articles";
 const TAGS_ENDPOINT = "/tags";
-const EDIT_POST_ENDPOINT = id => `/articles/${id}`;
-const DELETE_POSTS_ENDPOINT = id => `/posts/${id}`;
+const EDIT_POST_ENDPOINT = (id) => `/articles/${id}`;
+const DELETE_POSTS_ENDPOINT = (id) => `/posts/${id}`;
 
 export default {
   fetchPosts() {
     return $http.get(FETCH_POSTS_ENDPOINT);
+  },
+  fetchPost(id) {
+    return $http.get(EDIT_POST_ENDPOINT(id));
   },
   fetchTags() {
     return $http.get(TAGS_ENDPOINT);
@@ -23,8 +26,15 @@ export default {
       }
     });
   },
-  editPost(id, body) {
-    return $http.put(EDIT_POST_ENDPOINT(id), body);
+  editPost(id, title, description, body, tagList) {
+    return $http.put(EDIT_POST_ENDPOINT(id), {
+      article: {
+        title,
+        description,
+        body,
+        tagList
+      }
+    });
   },
   deletePost(id) {
     return $http.delete(DELETE_POSTS_ENDPOINT(id));
